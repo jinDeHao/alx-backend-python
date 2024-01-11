188ea2f74c2a88df25f45d70d5f5d63e6bcab211
omarIdhmaid <o.idhmaid@gmail.com>
2024-01-11 20:55:13 +0100

    UNION

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 """a type-annotated function"""
-from typing import Union
+from typing import Union, List
 
 
-def sum_mixed_list(mxd_lst: Union[int, float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """a type-annotated function"""
     sum: float = 0.0
     for n in mxd_lst:
         sum += float(n)
     return sum
+
+
+print(sum_mixed_list.__annotations__)
+mixed = [5, 4, 3.14, 666, 0.99]
+ans = sum_mixed_list(mixed)
+print(ans == sum(mixed))
+print("sum_mixed_list(mixed) returns {} which is a {}".format(ans, type(ans)))
