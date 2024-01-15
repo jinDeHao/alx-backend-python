02087f7094abf119ad5bf47aeb7b5a8aa6a4ee9d
omarIdhmaid <o.idhmaid@gmail.com>
2024-01-15 19:55:53 +0100

    basicas

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,1 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""async coroutine"""
+import asyncio
+from random import uniform
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """async function"""
+    t = uniform(0, max_delay)
+    await asyncio.sleep(t)
+    return t
