f0c7e87f6635752b1ceab13f93576d8c138b3455
omarIdhmaid <o.idhmaid@gmail.com>
2024-01-15 20:08:38 +0100

    true hhh

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,1 +1,16 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""async coroutine"""
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> list:
+    """wait many times"""
+    my_list: list = []
+    while True:
+        try:
+            if my_list[n - 1]:
+                return my_list
+        except IndexError:
+            t = await wait_random(max_delay)
+            my_list.append(t)
