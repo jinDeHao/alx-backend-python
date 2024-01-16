e4758177956c47c1b59225aced9239c8d04a4853
omarIdhmaid <o.idhmaid@gmail.com>
2024-01-16 20:40:27 +0100

    type of return in none return function

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import random
 from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """wait then yield"""
     for i in range(10):
         await asyncio.sleep(1)
