8669fe28847234cbb6d61705654c47a4d3d13e65
jinDeHao <o.idhmaid@gmail.com>
2024-02-05 23:25:11 +0100

    task 0

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,1 +1,23 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Parameterize a unit test
+"""
+import unittest
+from utils import access_nested_map
+from parameterized import parameterized
+from typing import Dict, Tuple, Any
+
+
+class TestAccessNestedMap(unittest.TestCase):
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self,
+                               nested_map: Dict[str, Any],
+                               path: Tuple[str],
+                               result):
+        """test access_nested_map function"""
+        self.assertEqual(access_nested_map(nested_map, path), result)
