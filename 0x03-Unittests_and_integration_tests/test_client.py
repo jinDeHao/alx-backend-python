aff4f95363b3d90ee9e8f3e32063738db7769c83
jinDeHao <o.idhmaid@gmail.com>
2024-02-06 01:58:27 +0100

    task 6

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -35,8 +35,26 @@ class TestGithubOrgClient(unittest.TestCase):
         """
         Mocking a property
         """
-        with patch('client.GithubOrgClient.org', new_callable=PropertyMock) as mock_property:
-            mock_property.return_value = {"de hao": "idhmaid", "repos_url": "omar"}
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock) as mock_property:
+            mock_property.return_value = {"de hao": "idhmaid",
+                                          "repos_url": "omar"}
             inst = GithubOrgClient("idh")
             result = inst._public_repos_url
             self.assertEqual(result, "omar")
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get: Mock):
+        """
+        More patching
+        """
+        mock_get.return_value = [{"name": "dehao",
+                                  "dehao asks": "are you stupid bro"}]
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock) as mock_property:
+            mock_property.return_value = "no repos_url for you, stupid!"
+            inst = GithubOrgClient("idh")
+            result = inst.public_repos()
+            self.assertEqual(result[0], "dehao")
+            mock_property.assert_called_once()
+            mock_get.assert_called_once()
