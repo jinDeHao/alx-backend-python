f018e9ee8bc8f319c9be6e8b50ad2350b3444d17
jinDeHao <o.idhmaid@gmail.com>
2024-02-06 01:32:24 +0100

    task 5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -4,7 +4,7 @@ Parameterize and patch as decorators
 """
 import unittest
 from utils import access_nested_map, get_json, memoize
-from unittest.mock import Mock, patch
+from unittest.mock import Mock, patch, PropertyMock
 from parameterized import parameterized
 from typing import Dict, Tuple, Any
 from client import GithubOrgClient
@@ -30,3 +30,13 @@ class TestGithubOrgClient(unittest.TestCase):
         url = f"https://api.github.com/orgs/{org}"
         GithubOrgClient(org).org
         mock_get.assert_called_once_with(url)
+
+    def test_public_repos_url(self):
+        """
+        Mocking a property
+        """
+        with patch('client.GithubOrgClient.org', new_callable=PropertyMock) as mock_property:
+            mock_property.return_value = {"repos_url": "omar"}
+            inst = GithubOrgClient("idh")
+            result = inst._public_repos_url
+            self.assertEqual(result, "omar")
